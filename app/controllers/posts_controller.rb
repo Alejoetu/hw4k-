d3ef1c20f154c35f645e900b73406f8e8b45365e
Alejo Etulain <alejo.etulain@kellogg.northwestern.edu>
2023-06-02 00:09:05 +0000

    2

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,20 @@
 class PostsController < ApplicationController
 
+  def index
+    @posts = Post.all
+  end
+
+  def show
+    @post = Post.find_by({ "id" => params["id"] })
+    @place = Place.find_by({ "id" => @post["place_id"] })
+  end
+  
   def new
-    @post = Post.new
-    @post.place_id = params["place_id"]
+    if @current_user
+      @posts = Post.where({ "user_id" => @current_user["id"] })
+      @post = Post.new
+      @post["place_id"] = @place["id"]
+    end
   end
 
   def create
@@ -10,9 +22,16 @@ class PostsController < ApplicationController
     @post["title"] = params["post"]["title"]
     @post["description"] = params["post"]["description"]
     @post["posted_on"] = params["post"]["posted_on"]
-    @post["place_id"] = params["post"]["place_id"]
+    @post["place_id"] = @post["place_id"]
+    @post["user_id"] = @current_user["id"]
     @post.save
     redirect_to "/places/#{@post["place_id"]}"
   end
-
+  def destroy
+    @post = Post.find_by({ "id" => params["id"] })
+    if @post["user_id"] == @current_user["id"]
+      @post.destroy
+    end
+    redirect_to "/places"
+  end
 end
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,27 @@ class SessionsController < ApplicationController
   end
 
   def create
+    @user = User.find_by({ "email" => params["email"] })
+    if @user
+      password_the_user_typed = params["password"]
+      password_in_the_database = @user["password"]
+      if BCrypt::Password.new(password_in_the_database) == password_the_user_typed
+        session["user_id"] = @user["id"]
+        flash["notice"] = "You are now logged-in."
+        redirect_to "/places"
+      else
+        flash["notice"] = "Sorry, invalid email or password."
+        redirect_to "/login"
+      end
+    else
+      flash["notice"] = "Sorry, invalid email or password."
+      redirect_to "/login"
+    end
   end
 
   def destroy
+    session["user_id"] = nil
+    flash["notice"] = "Goodbye."
+    redirect_to "/login"
   end
 end
-  
